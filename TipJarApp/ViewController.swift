47b6c72ea833509bca7795d339d2d3308da18d82
Piedad Garnica <Piedad@Piedads-iMac.hsd1.ca.comcast.net>
2015-05-08 15:58:22 -0700

    Initial Commit

---
+++ TipJarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipJarApp
+//
+//  Created by Piedad Garnica on 5/8/15.
+//  Copyright (c) 2015 Piedad Garnica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
